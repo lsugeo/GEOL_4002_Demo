974d3550d9038b5a631a846aee36d143a6cbc613
lsugeo <kluttrell@lsu.edu>
2026-01-23 15:31:19 -0600

    updates to demo script and add .gitignore

--- demostart_loadgps.m
+++ demostart_loadgps.m
@@ -9,7 +9,7 @@ clear % ALWAYS start your script be clearing the memory cache
 % If not, skip this line and just use a file you downloaded from the website 
 % by right-click SaveAs (if downloaded from website, be careful about the filename extension).
 %
-  !curl https://geodesy.unr.edu/gps_timeseries/IGS20/tenv3/NA/1LSU.NA.tenv3 > 1LSU.NA.tenv3
+  % !curl https://geodesy.unr.edu/gps_timeseries/IGS20/tenv3/NA/1LSU.NA.tenv3 > 1LSU.NA.tenv3
 
 %
 % Load the data
@@ -17,3 +17,16 @@ clear % ALWAYS start your script be clearing the memory cache
   fid=fopen('1LSU.NA.tenv3');
   C=textscan(fid,'%s %s %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f','headerlines',1);
   fclose(fid);
+
+%
+% Parse the data
+%
+  t=C{3};
+  x=C{9};
+
+%
+% Plot the data
+%
+  figure(1)
+  clf
+  plot(t,x,'.')
