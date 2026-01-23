11c16172e3871a727659dc39b587a28ee7bf6af2
lsugeo <kluttrell@lsu.edu>
2026-01-23 14:33:40 -0600

    add homework list and new gps demo start

---
+++ demostart_loadgps.m
@@ -0,0 +1,19 @@
+clear % ALWAYS start your script be clearing the memory cache
+
+
+%
+% Download the data directly from the source 
+% (note the station name and reference frame in the URL)
+%
+% If you have UNIX tools intalled, try downloading using the "curl" command.
+% If not, skip this line and just use a file you downloaded from the website 
+% by right-click SaveAs (if downloaded from website, be careful about the filename extension).
+%
+  !curl https://geodesy.unr.edu/gps_timeseries/IGS20/tenv3/NA/1LSU.NA.tenv3 > 1LSU.NA.tenv3
+
+%
+% Load the data
+%
+  fid=fopen('1LSU.NA.tenv3');
+  C=textscan(fid,'%s %s %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f','headerlines',1);
+  fclose(fid);
