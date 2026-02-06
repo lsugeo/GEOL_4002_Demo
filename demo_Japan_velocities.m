bf934866fcdd0179eebafb08add17f84ef803b24
lsugeo <kluttrell@lsu.edu>
2026-02-06 15:25:04 -0600

    Update demo_Japan_velocities.m

--- demo_Japan_velocities.m
+++ demo_Japan_velocities.m
@@ -5,17 +5,48 @@ clear % ALWAYS start your script be clearing the memory cache
 % Download the data directly from the source 
 % (note the station name and reference frame in the URL)
 %
-  websave('J175.OK.tenv3','https://geodesy.unr.edu/gps_timeseries/IGS20/tenv3/OK/J175.OK.tenv3');
+  % websave('J175.OK.tenv3','https://geodesy.unr.edu/gps_timeseries/IGS20/tenv3/OK/J175.OK.tenv3');
 
+%
+% Load the data - be careful what reference frame you choose!
+%
+  [t,x,xlocation,ylocation]=load_and_parse_one_GNSS('J175','OK');
 
+%
+% plot the time series
+%
+  figure(1)
+  clf
+  plot(t,x,'.-')
+  grid
 
+%
+% find all the points that are before the earthquake
+%
+  i_before=find(t < 2011.186);
 
+%
+% sanity check: plot the before points and make sure they're before
+%
+  hold on
+  plot(t(i_before),x(i_before),'.')
 
+%
+% Calculate velocity for entire time series, and for just the before part
+%  - print the values to the screen so we can read them and check them
+%
+  [vx,vy]=fit_velocity_one_GNSS(t,x);
+  [vx_before,vy_before]=fit_velocity_one_GNSS(t(i_before),x(i_before));
 
+  vx
+  vx_before
 
+%
+% find the points that we'll use for co-seismic displacement
+%
+  i_co=find(t>2011.186 & t<2011.190);
 
-
-
+  dx=x(i_co(2))-x(i_co(1))
 
 
 
@@ -23,8 +54,8 @@ clear % ALWAYS start your script be clearing the memory cache
 % FUNCTION DEFINITIONS START HERE
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-  function [t,x,xlocation,ylocation]=load_and_parse_one_GNSS(stationname)
-    filename=[stationname,'.NA.tenv3'];
+  function [t,x,xlocation,ylocation]=load_and_parse_one_GNSS(stationname,referenceplate)
+    filename=[stationname,'.',referenceplate,'.tenv3'];
 
     fid=fopen(filename);
     C=textscan(fid,'%s %s %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f','headerlines',1);
